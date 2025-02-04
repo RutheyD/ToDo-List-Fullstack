46b7a41f82366f37cd6d3fe40b7004a51af021d4
“rutheyd <r8507603@gmail.com>
2025-02-04 22:03:22 +0200

    :)

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -1,49 +1,90 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-// const apiUrl = "http://localhost:5047";
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+// // const apiUrl = "http://localhost:5047";
+// axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+// // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-axios.interceptors.response.use(
-  response => response, 
-  error => {
-    console.error('API Error:', error.response ? error.response.data : error.message);
-    return Promise.reject(error); 
-  }
-);
+// axios.interceptors.response.use(
+//   response => response, 
+//   error => {
+//     console.error('API Error:', error.response ? error.response.data : error.message);
+//     return Promise.reject(error); 
+//   }
+// );
+
+// export default {
+//   getTasks: async () => {
+//     console.log(process.env.REACT_APP_API_URL);
+
+//     const result = await axios.get(`/items`)    
+//     // const result = await axios.get(`${apiUrl}/items`)    
+//     return result.data;
+//   },
+
+//   addTask: async (name) => {
+    
+//     console.log('addTask', name)
+//       const result = await axios.post(`/items`,
+//        { Name:name,
+//         IsComplete:false
+//         })
+
+//     return {result};
+//   },
+
+//   setCompleted: async (id, isComplete) => {
+    
+//     console.log('setCompleted', {id, isComplete})
+//       const result = await axios.put(`/items/${id}?iscomplete=${isComplete}`, 
+//       {})
+//     return {result};
+//   },
+
+//   deleteTask: async (id) => {
+//     console.log('deleteTask')
+//     const result = await axios.delete(`/items/${id}`)
+ 
+//   }
+// };
+import axios from './api';
 
 export default {
+
   getTasks: async () => {
     console.log(process.env.REACT_APP_API_URL);
-
+    
     const result = await axios.get(`/items`)    
-    // const result = await axios.get(`${apiUrl}/items`)    
     return result.data;
   },
 
-  addTask: async (name) => {
-    
+
+  addTask: async(name)=>{
     console.log('addTask', name)
-      const result = await axios.post(`/items`,
-       { Name:name,
-        IsComplete:false
-        })
-
+    //TODO
+    const result = await axios.post(`/`,{
+      Name:name,
+      IsComplete:false
+    })    
     return {result};
   },
 
-  setCompleted: async (id, isComplete) => {
-    
+
+
+  setCompleted: async(id, isComplete)=>{
     console.log('setCompleted', {id, isComplete})
-      const result = await axios.put(`/items/${id}?iscomplete=${isComplete}`, 
-      {})
+    console.log('setCompleted', { id, isComplete });
+    const result = await axios.put(`/items/${id}?iscomplete=${isComplete}`, {
+    });
+      
     return {result};
   },
 
-  deleteTask: async (id) => {
+  deleteTask:async(id)=>{
     console.log('deleteTask')
-    const result = await axios.delete(`/items/${id}`)
- 
+
+    const result = await axios.delete(`/items/${id}`, {
+    });
+
   }
 };
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,72 +1,157 @@
-using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using TodoApi;
+
+// var builder = WebApplication.CreateBuilder(args);
+// //context-services
+ 
+//   builder.Services.AddDbContext<ToDoDbContext>(options =>
+//                 options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"), 
+//                 new MySqlServerVersion(new Version(8, 0, 0))));
+// //cors
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowAll",
+//         builder => builder.AllowAnyOrigin()
+//                           .AllowAnyMethod()
+//                           .AllowAnyHeader());
+// });
+// // builder.Services.AddCors(options =>
+// // {
+// //     options.AddDefaultPolicy(builder =>
+// //     {
+// //         builder.AllowAnyOrigin()
+// //                .AllowAnyMethod()
+// //                .AllowAnyHeader();
+// //     });
+// // });
+// //swagger
+// builder.Services.AddControllers();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+// var app = builder.Build();
+
+// // if (app.Environment.IsDevelopment())
+// // {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// // }
+// app.UseDeveloperExceptionPage(); // 👈 יראה לך את השגיאות האמיתיות
+
+// app.UseCors("AllowAll");
+// // app.UseCors();
+// app.UseHttpsRedirection();
+
+// app.MapGet("/", () => "Welcome to the ToDo API!");
+
+// app.MapGet("/items", async (ToDoDbContext context) =>{
+//  var items = await context.Items.ToListAsync();
+// return Results.Ok(items);
+// });
+
+// app.MapPut("/items/{id}",async(int id,bool iscomplete,ToDoDbContext context)=>{
+// var item = await context.Items.FindAsync(id);
+//     if (item is null) return Results.NotFound();
+
+//     item.IsComplete = iscomplete;
+
+//     await context.SaveChangesAsync();
+//     return Results.NoContent();
+// });
+// app.MapPost("/items",async([FromBody]Item item,ToDoDbContext context)=>{
+//  context.Add(item);
+//  await context.SaveChangesAsync();
+//  return Results.Created($"/items/{item.Id}", item);
+// });
+// app.MapDelete("/items/{id}", async (int id,ToDoDbContext context)=>{
+// var tmpItem = await context.Items.FindAsync(id);
+// if(tmpItem is null)
+//     return Results.NotFound("Item not found!!");
+//  context.Items.Remove(tmpItem);
+//  await context.SaveChangesAsync();
+//  return Results.NoContent();
+// });
+
+// app.Run();
+
+
 using Microsoft.EntityFrameworkCore;
 using TodoApi;
 
 var builder = WebApplication.CreateBuilder(args);
-//context-services
- 
-  builder.Services.AddDbContext<ToDoDbContext>(options =>
-                options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"), 
-                new MySqlServerVersion(new Version(8, 0, 0))));
-//cors
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        builder => builder.AllowAnyOrigin()
-                          .AllowAnyMethod()
-                          .AllowAnyHeader());
-});
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();
+
 // builder.Services.AddCors(options =>
 // {
-//     options.AddDefaultPolicy(builder =>
-//     {
-//         builder.AllowAnyOrigin()
-//                .AllowAnyMethod()
-//                .AllowAnyHeader();
-//     });
+//     options.AddPolicy("AllowAll",
+//         builder => builder.AllowAnyOrigin()
+//                           .AllowAnyMethod()
+//                           .AllowAnyHeader());
 // });
-//swagger
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-var app = builder.Build();
-// if (app.Environment.IsDevelopment())
-// {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-// }
-app.UseCors("AllowAll");
-// app.UseCors();
-app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Welcome to the ToDo API!");
-
-app.MapGet("/items", async (ToDoDbContext context) =>{
- var items = await context.Items.ToListAsync();
-return Results.Ok(items);
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
 });
 
-app.MapPut("/items/{id}",async(int id,bool iscomplete,ToDoDbContext context)=>{
-var item = await context.Items.FindAsync(id);
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("ToDoDB"),
+        new MySqlServerVersion(new Version(8, 0, 0))
+    ));
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors();
+// app.UseCors("AllowAll");
+
+
+app.MapGet("/", () => "TodoList API is running");
+
+app.MapGet("/items", async (ToDoDbContext db) =>
+{
+    var items = await db.Items.ToListAsync();
+    return Results.Ok(items);
+});
+
+app.MapGet("/items/{id}", async (int id, ToDoDbContext db) =>
+    await db.Items.FindAsync(id) is Item item ? Results.Ok(item) : Results.NotFound());
+
+
+app.MapPost("/", async (Item item, ToDoDbContext db) =>
+{
+    db.Add(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/items/{item.Id}", item);
+
+});
+
+app.MapPut("/items/{id}", async (int id, bool iscomplete, ToDoDbContext db) =>
+{
+    var item = await db.Items.FindAsync(id);
     if (item is null) return Results.NotFound();
 
     item.IsComplete = iscomplete;
 
-    await context.SaveChangesAsync();
+    await db.SaveChangesAsync();
     return Results.NoContent();
 });
-app.MapPost("/items",async([FromBody]Item item,ToDoDbContext context)=>{
- context.Add(item);
- await context.SaveChangesAsync();
- return Results.Created($"/items/{item.Id}", item);
-});
-app.MapDelete("/items/{id}", async (int id,ToDoDbContext context)=>{
-var tmpItem = await context.Items.FindAsync(id);
-if(tmpItem is null)
-    return Results.NotFound("Item not found!!");
- context.Items.Remove(tmpItem);
- await context.SaveChangesAsync();
- return Results.NoContent();
-});
 
+
+app.MapDelete("/items/{id}", async (int id, ToDoDbContext db) =>
+{
+    var item = await db.Items.FindAsync(id);
+    if (item is null) return Results.NotFound();
+
+    db.Items.Remove(item);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
 app.Run();
