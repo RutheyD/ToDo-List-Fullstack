c4cb97a9715d926548d4b9eb1e9292bab8a34610
“rutheyd <r8507603@gmail.com>
2025-02-04 22:30:46 +0200

    :(

--- TodoApi/ToDoDbContext.cs
+++ TodoApi/ToDoDbContext.cs
@@ -32,7 +32,7 @@ public partial class ToDoDbContext : DbContext
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
 
-            entity.ToTable("Items");
+            entity.ToTable("items");
 
             entity.Property(e => e.Name).HasMaxLength(100);
         });
